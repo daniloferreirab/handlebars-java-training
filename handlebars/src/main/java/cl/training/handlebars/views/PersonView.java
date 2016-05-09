a0fa7472599686c50d296b6e396076fd6a8236e0
daniloferreirab <daniloferreirabarra@gmail.com>
2016-05-09 11:41:06 -0300

    Revert "Revert "agregado person y contacts views, etc.""
    This reverts commit 3b233c98603a963579e6950e52ce8b3314a0bc3e.

---
+++ handlebars/src/main/java/cl/training/handlebars/views/PersonView.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package cl.training.handlebars.views;
+
+/**
+ * @author dferreib
+ *
+ */
+public class PersonView {
+	private String nombre;
+	private String edad;
+	private String direccion;
+	private String genero;
+	
+	
+	public PersonView(){
+		
+	}
+	
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the edad
+	 */
+	public String getEdad() {
+		return edad;
+	}
+	/**
+	 * @param edad the edad to set
+	 */
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+	/**
+	 * @return the direccion
+	 */
+	public String getDireccion() {
+		return direccion;
+	}
+	/**
+	 * @param direccion the direccion to set
+	 */
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	/**
+	 * @return the genero
+	 */
+	public String getGenero() {
+		return genero;
+	}
+	/**
+	 * @param genero the genero to set
+	 */
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	
+
+}
